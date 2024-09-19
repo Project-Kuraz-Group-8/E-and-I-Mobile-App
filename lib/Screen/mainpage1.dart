35c69e9f0fdee5d503444cd0f765c5e194be0b6e
mntc3434 <mnte3434@gmail.com>
2024-09-19 04:02:05 +0300

    finishing the people page and it has one error that happens when a users try to use a keyboard in co-founders

--- lib/Screen/mainpage1.dart
+++ lib/Screen/mainpage1.dart
@@ -120,7 +120,6 @@ class _PeopleScreenState extends State<PeopleScreen> {
               ],
             ),
             SizedBox(height: 16),
-            // Curved input box with custom icon
             TextField(
               decoration: InputDecoration(
                 prefixIcon: Icon(Icons.search),
@@ -153,14 +152,15 @@ class _PeopleScreenState extends State<PeopleScreen> {
       style: ElevatedButton.styleFrom(
         foregroundColor: isActive
             ? Colors.white // White text for active button
-            : Colors.black, backgroundColor: isActive
+            : Colors.black,
+        backgroundColor: isActive
             ? Color(0xFF264065) // Active color #264065
             : Color(0xFFD9D9D9),
         fixedSize: Size(156, 40), // Fixed width (156px) and height (40px)
         padding: EdgeInsets.zero,
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.all(
-             Radius.circular(8.0),
+            Radius.circular(8.0),
           ),
         ),
       ),
@@ -182,7 +182,7 @@ class _PeopleScreenState extends State<PeopleScreen> {
               decoration: BoxDecoration(
                 color: Colors.white, // #FFFFFF for profile container
                 borderRadius: BorderRadius.all(
-                   Radius.circular(16.0), // Border-radius (16px, 0px, 0px, 0px)
+                  Radius.circular(16.0), // Border-radius (16px, 0px, 0px, 0px)
                 ),
                 boxShadow: [
                   BoxShadow(
@@ -233,26 +233,16 @@ class _PeopleScreenState extends State<PeopleScreen> {
                   ),
                   Spacer(), // Push the buttons to the bottom of the container
                   Row(
-
                     mainAxisAlignment: MainAxisAlignment.end, // Align buttons to the left
                     children: [
-                      Row(
-                        children: [
-                          _buildSkillButton('Design', color: Colors.white, width: 66, height: 17,borderRadius: BorderRadius.circular(75.0),),
-                          SizedBox(width: 5), // 4px gap between the first two buttons
-                          _buildSkillButton('User Experience', color: Colors.white, width: 115, height: 17,borderRadius: BorderRadius.circular(75.0),),
-                        ],
-                      ),
-                      SizedBox(width: 9,),
+                      _buildSkillButton('Design', color: Colors.white),
+                      SizedBox(width: 5), // 4px gap between the first two buttons
+                      _buildSkillButton('User Experience', color: Colors.white),
+                      SizedBox(width: 9),
                       _buildSkillButton(
                         'Seed',
                         color: Color(0xFFEFE0F5), // Color for "Seed" button
                         textColor: Color(0xFFC76CEC),
-                        width: 66, // Width: 78px
-                        height: 17,
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(14.0), // Border-radius (16px, 0px, 0px, 0px)
-                        ),// Height: 32px
                       ),
                     ],
                   ),
@@ -266,58 +256,275 @@ class _PeopleScreenState extends State<PeopleScreen> {
   }
 
   Widget _buildCoFoundersList() {
-    return Center(child: Text('Co-founders list'));
+    return Expanded(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Recommended for you',
+            style: TextStyle(
+              fontSize: 26,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(height: 8),
+          Container(
+            height: 280, // Height for the profile container
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: 3, // Replace with actual data
+              itemBuilder: (context, index) {
+                return Container(
+                  width: 212.85, // Set width Hug (212.85px)
+                  height: 280, // Set height Hug (280px)
+                  padding: EdgeInsets.symmetric(vertical: 16, horizontal: 8),
+                  margin: EdgeInsets.only(right: 8),
+                  decoration: BoxDecoration(
+                    color: Color(0xFFFFFFFF), // Background color #FFFFFF
+                    borderRadius: BorderRadius.all(
+                       Radius.circular(16),
+                    ),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black12,
+                        blurRadius: 8,
+                        offset: Offset(0, 4),
+                      ),
+                    ],
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          CircleAvatar(
+                            radius: 40,
+                            backgroundImage:
+                            NetworkImage('https://via.placeholder.com/150'),
+                          ),
+                          Column(
+                            children: [
+                              Text(
+                                'Henok Tesfaye',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Row(
+                                children: [
+                                  Icon(Icons.location_on, size: 16),
+                                  SizedBox(width: 4),
+                                  Text('New York, USA'),
+                                ],
+                              ),
+                            ],
+                          ),
+
+                        ],
+                      ),
+                      SizedBox(height: 8),
+                      Text('Lorem ipsum dolor sit amet consectetur. Amet tellus sit magnis velit. adipiscing ipsum facilisi malesuada.'),
+                      Divider(color: Colors.grey),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          _buildSkillButton('Design', color: Color(0xFFFFFFFF)),
+                          _buildSkillButton(
+                            'User Experience',
+                            color: Color(0xFFFFFFFF),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          _buildSkillButton(
+                            'Connect',
+                            color: Color(0xFF264065),
+                            textColor: Colors.white,
+                          ),
+                          _buildSkillButton(
+                            'View Profile',
+                            color: Color(0xFFD9D9D9),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+          ),
+          SizedBox(height: 16),
+          Text(
+            'Connected',
+            style: TextStyle(
+              fontSize: 26,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(height: 8),
+          Expanded(
+            child: ListView.builder(
+              itemCount: 3, // Replace with actual data
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8.0),
+                  child: Container(
+                    width: 328, // Set width 328px
+                    height: 82, // Set height 82px
+                    padding: const EdgeInsets.fromLTRB(13, 8, 6, 0),
+                    decoration: BoxDecoration(
+                      color: Color(0xFFFFFFFF), // Background color #FFFFFF
+                      borderRadius: BorderRadius.all(
+                         Radius.circular(16),
+                      ),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black12,
+                          blurRadius: 8,
+                          offset: Offset(0, 4),
+                        ),
+                      ],
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: [
+                            CircleAvatar(
+                              radius: 25,
+                              backgroundImage: NetworkImage(
+                                  'https://via.placeholder.com/150'),
+                            ),
+                            SizedBox(width: 8,),
+                            Text('Hemen Daniel',style: TextStyle(fontSize: 25),),
+                            SizedBox(width: 70,),
+                            Align(
+                              alignment: Alignment.topRight,
+                              child: IconButton(
+                                icon: Icon(Icons.more_vert),
+                                onPressed: () {
+                                  // Action for the three dots button
+                                },
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(width: 4),
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+
+                            Row(
+                              children: [
+                                SizedBox(width: 50,),
+                                Icon(Icons.access_time, size: 16),
+                                SizedBox(width: 4),
+                                Text(
+                                  'Active',
+                                  style: TextStyle(
+                                    color: Color(0xFF0DADF2), // Text color for "Active"
+                                    backgroundColor: Color(0xFFECFAFD),
+                                  ),
+                                ),
+                                SizedBox(width: 50),
+
+                              ],
+                            ),
+
+                            Row(
+                              children: [
+                                Container(
+                                  padding: EdgeInsets.symmetric(
+                                      horizontal: 4, vertical: 2),
+                                  decoration: BoxDecoration(
+                                    color: Color(0xFFF8EDCD), // Background color for text
+                                    borderRadius: BorderRadius.circular(4),
+                                  ),
+                                  child: Row(
+                                    children: [
+                                      Icon(
+                                        Icons.lightbulb_outline,
+                                        size: 16,
+                                        color: Color(0xFFEBC351), // Icon color
+                                      ),
+                                      SizedBox(width: 4),
+                                      Text(
+                                        '3',
+                                        style: TextStyle(
+                                          color: Colors.black,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
   }
 
-  Widget _buildSkillButton(String label, {
-    required Color color,
-    Color? textColor,
-    required double width,
-    required double height, required  BorderRadius borderRadius,
-  }) {
-    return ElevatedButton(
-      onPressed: () {
-        // Add functionality for each button here
-        if (label == 'Design') {
-          print('Design button pressed');
-        } else if (label == 'User Experience') {
-          print('User Experience button pressed');
-        } else if (label == 'Seed') {
-          print('Seed button pressed');
-        }
-      },
-      style: ElevatedButton.styleFrom(
-        foregroundColor: textColor ?? Colors.black, backgroundColor: color,
-        fixedSize: Size(width, height), // Dynamic width and height
-        padding: EdgeInsets.symmetric(horizontal: 9, vertical: 6), // Padding
-        shape: RoundedRectangleBorder(
-          borderRadius: borderRadius, // Border-radius (75px on left side)
-          side: BorderSide(
-            color: Color(0xFF909090), // Border color #909090
-            width: 0.75,
-          ),
+  Widget _buildSkillButton(
+      String label, {
+        Color? color,
+        Color? textColor,
+      }) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+      decoration: BoxDecoration(
+        color: color ?? Colors.grey[300],
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Text(
+        label,
+        style: TextStyle(
+          color: textColor ?? Colors.black,
+          fontWeight: FontWeight.bold,
         ),
       ),
-      child: Text(label),
     );
   }
 }
 
-// Custom icon for three minus symbols decreasing in size
 class CustomMinusIcon extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Container
-          (width: 20, height:
-        2, color: Colors.black),
-        SizedBox(height: 4),
-        Container(width: 16, height: 2, color: Colors.black),
-        SizedBox(height: 4),
-        Container(width: 12, height: 2, color: Colors.black),
-      ],
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            width: 16,
+            height: 2,
+            color: Colors.black,
+          ),
+          SizedBox(height: 4),
+          Container(
+            width: 12,
+            height: 2,
+            color: Colors.black,
+          ),
+          SizedBox(height: 4),
+          Container(
+            width: 8,
+            height: 2,
+            color: Colors.black,
+          ),
+        ],
+      ),
     );
   }
 }
